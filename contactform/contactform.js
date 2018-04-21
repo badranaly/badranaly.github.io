6f60c67fc2b7b861385093c312b14555944b6f82
Aly Badran <aly2cool@hotmail.com>
2018-04-21 06:11:02 -0400

    more contact

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -92,7 +92,7 @@ jQuery(document).ready(function($) {
     else var str = $(this).serialize();
     $.ajax({
       type: "POST",
-      url: "badranaly.github.io/contactform/contactform.php",
+      url: "./contactform.php",
       data: str,
       success: function(msg) {
         // alert(msg);
