792ac43661f3f0521e45577b7db985500ccdec38
Aly Badran <aly2cool@hotmail.com>
2018-04-23 06:56:11 -0400

    new php script

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -95,7 +95,6 @@ jQuery(document).ready(function($) {
     if (ferror) return false;
     else var str = $(this).serialize();
     $.ajax({
-      console.log('inside ajax call');
       type: "POST",
       url: "./contactform.php",
       data: str,
